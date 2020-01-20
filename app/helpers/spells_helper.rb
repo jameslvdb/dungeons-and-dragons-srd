a49f9b8cfd924552d8bc8451bc4c816397f08b4b
James Vanderburg <james@vanderburg.org>
2020-01-19 20:01:57 -0600

    Update spell/show view

--- app/helpers/spells_helper.rb
+++ app/helpers/spells_helper.rb
@@ -1,2 +1,15 @@
 module SpellsHelper
+  def components(spell)
+    result = []
+    result.append('V') if spell.verbal
+    result.append('S') if spell.somatic
+    if !spell.material.nil?
+      if spell.material[0] == '{'
+        result.append("M (#{eval(spell.material)['text']})")
+      elsif spell.material
+        result.append("M (#{spell.material})")
+      end
+    end
+    result.join(', ')
+  end
 end
