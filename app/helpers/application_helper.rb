a62e320b8246c45f9a4fa4f8bbc15bd538cdd618
James Vanderburg <james@vanderburg.org>
2020-01-20 18:17:59 -0600

    Turn off Creature link substitution for now

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def substitute_links(str)
     # use working creature links
-    str.gsub!(/{@creature (\w+\s?\w*)}/) do
-      crt = Creature.find_by(name: $1.downcase)
-      link_to crt.name.downcase, creature_path(crt)
-    end
+    # str.gsub!(/{@creature (\w+\s?\w*)}/) do
+    #   crt = Creature.find_by(name: $1.downcase)
+    #   link_to crt.name.downcase, creature_path(crt)
+    # end
 
     str.gsub!(/{@spell (\w+\s?\w*)}/) do
       spell = Spell.find_by(name: $1.downcase)
