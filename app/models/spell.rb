dd36d55cc38ae71f35e34b48175b5f83367a0935
James Vanderburg <james@vanderburg.org>
2020-01-20 21:34:57 -0600

    Add spell model validations and tests

--- app/models/spell.rb
+++ app/models/spell.rb
@@ -1,20 +1,21 @@
 class Spell < ApplicationRecord
+  SPELL_SCHOOLS = {
+    'A' => 'Abjuration',
+    'C' => 'Conjuration',
+    'D' => 'Divination',
+    'E' => 'Enchantment',
+    'V' => 'Evocation',
+    'I' => 'Illusion',
+    'N' => 'Necromancy',
+    'T' => 'Transmutation'
+  }
+
   validates :name, presence: true
-  validates :level, presence: true
-  validates :school, presence: true
+  validates :level, presence: true, inclusion: { in: 0..9, message: "must be between 0 and 9" }
+  validates :school, presence: true, inclusion: { in: SPELL_SCHOOLS.values }
   validates :casting_time, presence: true
   validates :range, presence: true
   validates :duration, presence: true
   validates :description, presence: true
-
-  SPELL_SCHOOLS = {
-  'A' => 'Abjuration',
-  'C' => 'Conjuration',
-  'D' => 'Divination',
-  'E' => 'Enchantment',
-  'V' => 'Evocation',
-  'I' => 'Illusion',
-  'N' => 'Necromancy',
-  'T' => 'Transmutation'
-}
+  validates :name, uniqueness: true
 end
--- test/models/spell_test.rb
+++ test/models/spell_test.rb
@@ -1,7 +1,71 @@
 require 'test_helper'
 
 class SpellTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # Specify the fixtures to load
+  fixtures :spells
+
+  test "spell attributes must not be empty" do
+    spell = Spell.new
+    assert spell.invalid?
+    assert spell.errors[:name].any?
+    assert spell.errors[:description].any?
+    assert spell.errors[:level].any?
+    assert spell.errors[:school].any?
+    assert spell.errors[:casting_time].any?
+    assert spell.errors[:range].any?
+    assert spell.errors[:duration].any?
+  end
+
+  test "spell level must be between 0 and 9" do
+    spell = Spell.new(name: "Spell name",
+                          description: "desc",
+                          school: "Evocation",
+                          casting_time: "1 action",
+                          range: "60 feet",
+                          duration: "1 minute"
+                        )
+    spell.level = -1
+    assert spell.invalid?
+    assert_equal ["must be between 0 and 9"], spell.errors[:level]
+
+    spell.level = 10
+    assert spell.invalid?
+    assert_equal ["must be between 0 and 9"], spell.errors[:level]
+
+    spell.level = 1
+    assert spell.valid?
+  end
+
+  def new_spell(school)
+    Spell.new(name: "Spell name", description: "desc", level: 1, school: school,
+              casting_time: "1 action", range: "60 feet", duration: "1 minute")
+  end
+
+  test "spell school" do
+    # D&D schools of magic
+    ok = %w[Abjuration Conjuration Divination Enchantment Evocation Illusion Necromancy Transmutation]
+    # Skyrim-only schools of magic
+    bad = %w[Destruction Alteration Restoration]
+
+    ok.each do |school|
+      # the trailing string is written along with the error message if the assertion fails
+      assert new_spell(school).valid?, "#{school} shouldn't be invalid"
+    end
+
+    bad.each do |school|
+      assert new_spell(school).invalid? "#{school} shouldn't be valid"
+    end
+  end
+
+  test "spell is not valid without a unique name" do
+    spell = Spell.new(name: spells(:magic_missile).name,
+                          description: "desc",
+                          level: 1,
+                          school: "Evocation",
+                          casting_time: "1 action",
+                          range: "60 feet",
+                          duration: "1 minute")
+    assert spell.invalid?
+    assert_equal ["has already been taken"], spell.errors[:name]
+  end
 end
