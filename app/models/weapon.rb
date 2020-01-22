eecadb04fcf0ea3aedf770d348de3c44bf4c981c
James Vanderburg <james@vanderburg.org>
2020-01-21 23:55:21 -0600

    Update fixtures and tests for weapons

--- app/models/weapon.rb
+++ app/models/weapon.rb
@@ -4,4 +4,5 @@ class Weapon < ApplicationRecord
   # TODO: refactor validations to work while allowing the exceptions for net and sling
   validates :name, :weapon_type, :category, :value, :weight, presence: true
   validates :value, :weight, numericality: { greater_than_or_equal_to: 0 }
+  validates :name, uniqueness: true
 end
--- test/controllers/weapons_controller_test.rb
+++ test/controllers/weapons_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class WeaponsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @weapon = weapons(:one)
+    @weapon = weapons(:shortsword)
   end
 
   test "should get index" do
@@ -17,7 +17,7 @@ class WeaponsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create weapon" do
     assert_difference('Weapon.count') do
-      post weapons_url, params: { weapon: { category: @weapon.category, damage: @weapon.damage, damage_type: @weapon.damage_type, description: @weapon.description, name: @weapon.name, range: @weapon.range, two_handed_damage: @weapon.two_handed_damage, value: @weapon.value, weapon_type: @weapon.weapon_type, weight: @weapon.weight } }
+      post weapons_url, params: { weapon: { category: @weapon.category, damage: @weapon.damage, damage_type: @weapon.damage_type, description: @weapon.description, name: "new weapon", range: @weapon.range, two_handed_damage: @weapon.two_handed_damage, value: @weapon.value, weapon_type: @weapon.weapon_type, weight: @weapon.weight } }
     end
 
     assert_redirected_to weapon_url(Weapon.last)
