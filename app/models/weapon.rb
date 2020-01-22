74c8c74a761c0957620cafcde89ae4c077a0a728
James Vanderburg <james@vanderburg.org>
2020-01-21 18:24:34 -0600

    Create has_and_belongs_to_many associations

--- app/models/weapon.rb
+++ app/models/weapon.rb
@@ -1,2 +1,3 @@
 class Weapon < ApplicationRecord
+  has_and_belongs_to_many :weapon_properties
 end
--- app/models/weapon_property.rb
+++ app/models/weapon_property.rb
@@ -1,4 +1,6 @@
 class WeaponProperty < ApplicationRecord
+  has_and_belongs_to_many :weapons
+  
   validates :name, :abbreviation, presence: true
   validates :name, :abbreviation, uniqueness: true
 end
