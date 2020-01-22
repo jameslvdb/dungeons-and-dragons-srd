fda48474096db4dfb88bc94e51a098308e7ea940
James Vanderburg <james@vanderburg.org>
2020-01-21 18:07:40 -0600

    Clean up Spell model validation statements

--- app/models/spell.rb
+++ app/models/spell.rb
@@ -10,12 +10,8 @@ class Spell < ApplicationRecord
     'T' => 'Transmutation'
   }
 
-  validates :name, presence: true
-  validates :level, presence: true, inclusion: { in: 0..9, message: "must be between 0 and 9" }
-  validates :school, presence: true, inclusion: { in: SPELL_SCHOOLS.values }
-  validates :casting_time, presence: true
-  validates :range, presence: true
-  validates :duration, presence: true
-  validates :description, presence: true
+  validates :name, :level, :school, :casting_time, :range, :duration, :description, presence: true
+  validates :level, inclusion: { in: 0..9, message: "must be between 0 and 9" }
+  validates :school, inclusion: { in: SPELL_SCHOOLS.values }
   validates :name, uniqueness: true
 end
