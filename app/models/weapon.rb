cb3cc3df53d6eb70e40fa7d342e17d91022e61b0
James Vanderburg <james@vanderburg.org>
2020-01-21 21:47:54 -0600

    Add Weapon model validations

--- app/models/weapon.rb
+++ app/models/weapon.rb
@@ -1,3 +1,6 @@
 class Weapon < ApplicationRecord
   has_and_belongs_to_many :weapon_properties
+
+  validates :name, :weapon_type, :damage_type, :damage, :category, :value, :weight, presence: true
+  validates :value, :weight, numericality: { greater_than: 0 }
 end
