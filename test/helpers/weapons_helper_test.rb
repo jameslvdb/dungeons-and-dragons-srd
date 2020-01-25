bac673805a251272f37fc5ff6319a6c6a9ef8439
James Vanderburg <james@vanderburg.org>
2020-01-25 16:00:23 -0600

    Add test for the render_properties helper

--- test/helpers/weapons_helper_test.rb
+++ test/helpers/weapons_helper_test.rb
@@ -8,4 +8,12 @@ class WeaponsHelperTest < ActionView::TestCase
   test 'should return 1d8 slashing' do
     assert_equal '1d8 slashing', render_damage(weapons(:longsword))
   end
+
+  test 'should return Ammunition (range 80/320)' do
+    assert_equal 'Ammunition (range 80/320)', render_properties(weapons(:shortbow))
+  end
+
+  test 'should return Versatile (1d10)' do
+    assert_equal 'Versatile (1d10)', render_properties(weapons(:longsword))
+  end
 end
