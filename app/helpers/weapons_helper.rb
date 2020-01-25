103487a11c7b799a9ba607655f32937bab28531e
James Vanderburg <james@vanderburg.org>
2020-01-25 15:48:34 -0600

    Change weapons/index view to match book

--- app/helpers/weapons_helper.rb
+++ app/helpers/weapons_helper.rb
@@ -2,7 +2,22 @@ module WeaponsHelper
   def render_damage(weapon)
     result = ''
     result += "#{weapon.damage} #{weapon.damage_type&.downcase}"
-    result += " (#{weapon.two_handed_damage})" if weapon.two_handed_damage
     result
+  end
+
+  def render_properties(weapon)
+    properties = weapon.weapon_properties.pluck(:name)
+    return if properties.empty?
+    properties.each do |property|
+      if property == 'versatile'
+        puts 'versatile weapon'
+        property << " (#{weapon.two_handed_damage})"
+        puts property
+      elsif property == 'thrown' || property == 'ammunition'
+        property << " (range #{weapon.range})"
+      end
+    end
+    properties.first.capitalize!
+    properties.join(', ')
   end
 end
