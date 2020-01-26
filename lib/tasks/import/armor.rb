99267d2f836f64e075bc59fde954e02fcd43db73
James Vanderburg <james@vanderburg.org>
2020-01-25 20:01:57 -0600

    Remove print statements from armor import

--- lib/tasks/import/armor.rb
+++ lib/tasks/import/armor.rb
@@ -7,13 +7,11 @@ all_armor = parsed['armor']
 
 all_armor.each do |a|
   armor = Armor.find_or_initialize_by(name: a["name"])
-  puts armor.name
   armor.ac = a["ac"]
   armor.armor_type = Armor::ARMOR_TYPES[a["type"]]
   armor.weight = a["weight"].to_i
   # Assign strength_requirement if the field exists
   a["strength"] ? armor.strength_requirement = a["strength"] : armor.strength_requirement = 0
-  puts armor.name
   armor.value = a["value"].split[0].to_i * 100
   armor.description = a["entries"].first if a["entries"].first.class == String
   # Set stealth_disadvantage to true if it exists
