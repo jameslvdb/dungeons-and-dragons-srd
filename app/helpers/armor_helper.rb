1de1d5d433b1ed79314d54eb300dd0ffc6b64b27
James Vanderburg <james@vanderburg.org>
2020-01-25 20:17:09 -0600

    Add render_armor_class helper

--- app/helpers/armor_helper.rb
+++ app/helpers/armor_helper.rb
@@ -1,2 +1,10 @@
 module ArmorHelper
+  def render_armor_class(armor)
+    if armor.armor_type == 'light'
+      return "#{armor.ac} + Dex modifier"
+    elsif armor.armor_type == 'medium'
+      return "#{armor.ac} + Dex modifier (max 2)"
+    end
+    armor.ac
+  end
 end
