ab02d9e84d2021f2208c52c6929393f243bd4675
James Vanderburg <james@vanderburg.org>
2020-01-26 13:20:23 -0600

    Add Armor model tests

--- app/models/armor.rb
+++ app/models/armor.rb
@@ -7,7 +7,8 @@ class Armor < ApplicationRecord
 
   validates :name, :armor_type, :ac, :strength_requirement, :weight, :value, :description, presence: true
   validates :stealth_disadvantage, inclusion: { in: [ true, false ] }
-  validates :value, :weight, numericality: { greater_than_or_equal_to: 0 }
+  validates :value, :weight, :ac, numericality: { greater_than_or_equal_to: 0 }
+  validates :strength_requirement, inclusion: { in: 0..20, message: "must be between 0 and 20"}
   validates :name, uniqueness: true
   validates :armor_type, inclusion: { in: ARMOR_TYPES.values }
 end
--- test/models/armor_test.rb
+++ test/models/armor_test.rb
@@ -1,7 +1,60 @@
 require 'test_helper'
 
 class ArmorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # Specify the fixtures to load
+  fixtures :armor
+
+  test "armor attributes must not be empty" do
+    armor = Armor.new
+    assert armor.invalid?
+    assert armor.errors[:name].any?
+    assert armor.errors[:description].any?
+    assert armor.errors[:ac].any?
+    assert armor.errors[:strength_requirement].any?
+    assert armor.errors[:stealth_disadvantage].any?
+    assert armor.errors[:weight].any?
+    assert armor.errors[:value].any?
+  end
+
+  test "weight, value, and ac attributes must be non-negative" do
+    armor = Armor.new(armor(:one).attributes)
+    armor.name = "asdf" # new name to get past uniqueness validation
+
+    integer_attributes = %i[ac value weight]
+    integer_attributes.each do |attr|
+      armor.send("#{attr}=", -1)
+      assert armor.invalid?
+      assert_equal ["must be greater than or equal to 0"], armor.errors[attr]
+      armor.send("#{attr}=", 0)
+      assert armor.valid?
+    end
+  end
+
+  test "strength requirement must be between 0 and 20" do
+    armor = Armor.new(armor(:one).attributes)
+    armor.name = "asdf" # new name to get past uniqueness validation
+
+    armor.strength_requirement = -1
+    assert armor.invalid?
+    assert_equal ["must be between 0 and 20"], armor.errors[:strength_requirement]
+
+    armor.strength_requirement = 21
+    assert armor.invalid?
+    assert_equal ["must be between 0 and 20"], armor.errors[:strength_requirement]
+
+    armor.strength_requirement = 10
+    assert armor.valid?
+  end
+
+  test "armor type must be light, medium, or heavy" do
+    armor = Armor.new(armor(:one).attributes)
+    armor.name = "asdf" # new name to get past uniqueness validation
+
+    armor.armor_type = "invalid type"
+    assert armor.invalid?
+    assert_equal ["is not included in the list"], armor.errors[:armor_type]
+
+    armor.armor_type = Armor::ARMOR_TYPES.values.sample
+    assert armor.valid?
+  end
 end
