9c390d7cd20ebdfe4f9d78c3f36e9bbef22bc2d6
James Vanderburg <james@vanderburg.org>
2020-01-26 20:34:31 -0600

    Add constants to prep for Monster model

---
+++ app/models/alignment.rb
@@ -0,0 +1,15 @@
+class Alignment
+  ALIGNMENTS = {
+    'LG' => 'lawful good',
+    'NG' => 'neutral good',
+    'CG' => 'chaotic good',
+    'LN' => 'lawful neutral',
+    'N' => 'neutral',
+    'CN' => 'chaotic neutral',
+    'LE' => 'lawful evil',
+    'NE' => 'neutral evil',
+    'CE' => 'chaotic evil',
+    'U' => 'unaligned',
+    'A' => 'any alignment'
+  }
+end
---
+++ app/models/size.rb
@@ -0,0 +1,10 @@
+class Size
+  SIZES = {
+    'T' => 'Tiny',
+    'S' => 'Small',
+    'M' => 'Medium',
+    'L' => 'Large',
+    'H' => 'Huge',
+    'G' => 'Gargantuan'
+  }
+end
