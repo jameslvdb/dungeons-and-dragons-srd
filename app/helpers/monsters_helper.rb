4269b9af8fc9325f3782c76ee53e35203ed49f99
James Vanderburg <james@vanderburg.org>
2020-01-28 21:16:53 -0600

    Style monster/index and monster/show

--- app/helpers/monsters_helper.rb
+++ app/helpers/monsters_helper.rb
@@ -1,2 +1,13 @@
 module MonstersHelper
+  def render_size_type_alignment(monster)
+    "#{monster.size} #{monster.monster_type}, #{monster.alignment}"
+  end
+
+  def render_hp(monster)
+    "#{monster.hp} (#{monster.hp_formula})"
+  end
+
+  def ability(score)
+    "#{score} (#{score > 9 ? '+' : ''}#{(score - 10) / 2})"
+  end
 end
--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,2 +1,12 @@
 class Monster < ApplicationRecord
+  def ability_scores
+    return {
+      str: strength,
+      dex: dexterity,
+      con: constitution,
+      int: intelligence,
+      wis: wisdom,
+      cha: charisma
+    }
+  end
 end
