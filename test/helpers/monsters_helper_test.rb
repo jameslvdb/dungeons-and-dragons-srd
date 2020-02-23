a8dd199358194fa37fb2690cad8201b4a94913f2
James Vanderburg <james@vanderburg.org>
2020-02-23 01:11:08 -0600

    Add render_hp tests

--- test/helpers/monsters_helper_test.rb
+++ test/helpers/monsters_helper_test.rb
@@ -10,4 +10,14 @@ class MonstersHelperTest < ActionView::TestCase
     monster = monsters(:demon)
     assert_equal 'large demon, chaotic evil', render_size_type_alignment(monster)
   end
+
+  test "should render HP as 75 (10d8 + 30)" do
+    monster = monsters(:humanoid)
+    assert_equal '75 (10d8 + 30)', render_hp(monster)
+  end
+
+  test "should render HP as 68 (8d10 + 24)" do
+    monster = monsters(:demon)
+    assert_equal '68 (8d10 + 24)', render_hp(monster)
+  end
 end
