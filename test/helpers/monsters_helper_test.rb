237ebf845a146f72551724e3b8e765a0e38f7e7c
James Vanderburg <james@vanderburg.org>
2020-02-23 01:04:02 -0600

    Refactor fixture for helper test
    Refactored the fixture to reflect what I wanted to test with in the
    helper tests.

--- test/helpers/monsters_helper_test.rb
+++ test/helpers/monsters_helper_test.rb
@@ -1,5 +1,8 @@
 require 'test_helper'
 
 class MonstersHelperTest < ActionView::TestCase
-  
+  test "should return 'medium humanoid, lawful good'" do
+    monster = monsters(:humanoid)
+    assert_equal 'medium humanoid, lawful good', render_size_type_alignment(monster)
+  end
 end
